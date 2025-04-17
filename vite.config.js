3d0862eb39dd78716206daa16ac3f83f9ec46f86
CodeWithDanyal <danyal.devpro@gmail.com>
2025-04-17 20:18:18 +0500

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    chunkSizeWarningLimit: 1000, // This sets the warning limit to 1MB (1000KB)
+    chunkSizeWarningLimit: 1000, // Sets the warning limit to 1MB (1000KB)
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
   },
-   plugins: [react()],
+  plugins: [react()],
+  base: '/Danyal-Khan-Portfolio/', // Set this to your GitHub repo name
 })
